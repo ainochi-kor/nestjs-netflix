16c788f073bb118b1920020252f1c7035e03c9b3
ainochi-kor <kthrkdals@gmail.com>
2025-04-20 01:23:17 +0900

    4.2.10 Query Parameter 사용해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -32,8 +33,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getMovies() {
-    return this.#movies;
+  getMovies(@Query('title') title?: string) {
+    if (!title) {
+      return this.#movies;
+    }
+
+    return this.#movies.filter((movie) => movie.title.startsWith(title));
   }
 
   @Get(':id')
