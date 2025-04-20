387e4ee965c50cd51a28ec22658061c37ff7c4eb
ainochi-kor <kthrkdals@gmail.com>
2025-04-20 14:20:51 +0900

    4.3.03 GET movie 로직 service로 전환하기

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Movie {
+  id: number;
+  title: string;
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  #movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+  #idCounter = 3;
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.#movies;
+    }
+
+    return this.#movies.filter((movie) => movie.title.startsWith(title));
   }
 }
