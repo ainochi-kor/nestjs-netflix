5d608d98ba3183bae28f9622038e5d6731e325a0
ainochi-kor <kthrkdals@gmail.com>
2025-05-06 19:21:05 +0900

    feat: update getManyMovies method to support title filtering

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -3,7 +3,7 @@ import { UpdateMovieDto } from './dto/update-movie.dto';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entity/movie.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 
 @Injectable()
 export class MovieService {
@@ -12,9 +12,19 @@ export class MovieService {
     private readonly movieRepository: Repository<Movie>,
   ) {}
 
-  getManyMovies(title?: string) {
-    // 나중에 title 필터 기능 추가하기
-    return this.movieRepository.find();
+  async getManyMovies(title?: string) {
+    if (!title) {
+      return [
+        await this.movieRepository.find(),
+        await this.movieRepository.count(),
+      ];
+    }
+
+    return this.movieRepository.findAndCount({
+      where: {
+        title: Like(`%${title}%`),
+      },
+    });
   }
 
   async getMovieById(id: number) {
