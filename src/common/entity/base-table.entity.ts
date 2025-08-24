7026f6b78d89833a4e8f5c8a592fe4d791b5ede6
ainochi-kor <kthrkdals@gmail.com>
2025-08-25 00:27:01 +0900

    Serialization 적용하기

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,16 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude() // 추출 시, createdAt 제외
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude() // 추출 시, updatedAt 제외
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude() // 추출 시, version 제외
   version: number;
 }
