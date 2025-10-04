0829bd89a93433b7ceb22ea1029d8c21c9532c50
ainochi-kor <kthrkdals@gmail.com>
2025-10-04 22:15:52 +0900

    ApiProperty 어노테이션 알아보기

--- src/common/dto/cursor-pagination.dto.ts
+++ src/common/dto/cursor-pagination.dto.ts
@@ -1,8 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class CursorPaginationDto {
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    description: '페이지네이션 커서',
+    example: 'eyJ2YWx1ZXMiOnsiaWQiOjF9LCJvcmRlciI6WyJpZF9ERVNDIl19',
+  })
   cursor?: string;
 
   @IsArray()
@@ -10,9 +16,20 @@ export class CursorPaginationDto {
     each: true,
   })
   @IsOptional()
+  @ApiProperty({
+    description: '내림차 또는 오름차 정렬',
+    example: ['id_DESC'],
+  })
+  @Transform(({ value }: { value: string | string[] }) =>
+    Array.isArray(value) ? value : [value],
+  )
   order: string[] = ['id_DESC'];
 
   @IsInt()
   @IsOptional()
+  @ApiProperty({
+    description: '가져올 데이터 수',
+    example: 5,
+  })
   take: number = 5;
 }
--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
@@ -10,14 +11,26 @@ import {
 export class CreateMovieDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 제목',
+    example: '겨울왕국',
+  })
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 상세 설명',
+    example: '겨울왕국은 2013년에 개봉한 디즈니 애니메이션 영화입니다.',
+  })
   detail: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({
+    description: '감독 객체 ID',
+    example: 1,
+  })
   directorId: number;
 
   @IsArray()
@@ -29,8 +42,16 @@ export class CreateMovieDto {
     },
   )
   @Type(() => Number)
+  @ApiProperty({
+    description: '장르 객체 ID 배열',
+    example: [1, 2],
+  })
   genreIds: number[];
 
   @IsString()
+  @ApiProperty({
+    description: '영화 파일 이름',
+    example: 'movie-1679658493843.mp4',
+  })
   movieFileName: string;
 }
