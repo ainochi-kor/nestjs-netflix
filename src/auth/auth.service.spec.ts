0db02e951e43ded7f18c72725c6cc2238e41f22e
ainochi-kor <kthrkdals@gmail.com>
2025-10-05 17:55:09 +0900

    AuthService 테스트하기 1

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -7,6 +7,7 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { BadRequestException } from '@nestjs/common';
 
 const mockedUserRepository = {
   findOne: jest.fn(),
@@ -73,7 +74,65 @@ describe('AuthService', () => {
     userService = module.get<UserService>(UserService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(authService).toBeDefined();
+  });
+
+  describe('blockToken', () => {
+    it('should block a token', async () => {
+      const token = 'token';
+      const payload = {
+        exp: Math.floor(Date.now() / 1000) + 60,
+      };
+
+      const decodeSpyOn = jest
+        .spyOn(jwtService, 'decode')
+        .mockReturnValue(payload);
+
+      await authService.blockToken(token);
+
+      expect(decodeSpyOn).toHaveBeenCalledWith(token);
+      expect(cacheManager.set).toHaveBeenCalledWith(
+        `BLOCK_TOKEN_${token}`,
+        payload,
+        expect.any(Number),
+      );
+    });
+  });
+
+  describe('parseBasicToken', () => {
+    it('should parse a valid basic token', () => {
+      const rawToken =
+        'Basic ' + Buffer.from('username:password').toString('base64');
+      const result = authService.parseBasicToken(rawToken);
+      const decode = { email: 'username', password: 'password' };
+
+      expect(result).toEqual(decode);
+    });
+
+    it('should throw BadRequestException for invalid token format', () => {
+      const rawToken = 'InvalidTokenFormat';
+      expect(() => authService.parseBasicToken(rawToken)).toThrow(
+        BadRequestException,
+      );
+    });
+
+    it('should throw BadRequestException for invalid basic token format', () => {
+      const rawToken = 'Bearer InvalidTokenFormat';
+      expect(() => authService.parseBasicToken(rawToken)).toThrow(
+        BadRequestException,
+      );
+    });
+
+    it('should throw BadRequestException for invalid basic token format', () => {
+      const rawToken = 'basic InvalidTokenFormat';
+      expect(() => authService.parseBasicToken(rawToken)).toThrow(
+        BadRequestException,
+      );
+    });
   });
 });
