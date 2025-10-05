f37281e4ee64d4746cd95fcac16595400f60577d
ainochi-kor <kthrkdals@gmail.com>
2025-10-05 17:38:13 +0900

    Auth Module Unit Test 세팅하기

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockedUserRepository = {
+  findOne: jest.fn(),
+};
+
+const mockedConfigService = {
+  get: jest.fn(),
+};
+
+const mockedJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+  decode: jest.fn(),
+};
+
+const mockedCacheManager = {
+  set: jest.fn(),
+};
+
+const mockedUserService = {
+  create: jest.fn(),
+};
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userRepository: Repository<User>;
+  let configService: ConfigService;
+  let jwtService: JwtService;
+  let cacheManager: Cache;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockedUserRepository,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockedCacheManager,
+        },
+        {
+          provide: UserService,
+          useValue: mockedUserService,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    configService = module.get<ConfigService>(ConfigService);
+    jwtService = module.get<JwtService>(JwtService);
+    cacheManager = module.get<Cache>('CACHE_MANAGER');
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
